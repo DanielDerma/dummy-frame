2a9e2046ee7a5e8716cf67527b4c9ece862203a2
danielderma <danielderma.contact@gmail.com>
2023-04-08 14:26:24 -0600

    dpelou

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -6,13 +6,11 @@ const inter = Inter({ subsets: ["latin"] });
 export default function Home() {
   return (
     <main className="flex min-h-screen flex-col items-center justify-between p-24">
-      <audio
-        autoPlay="wCdZKQXZHGBIuMqykoAwBLvTJhqRceQL2IRLOqVkNXdOHJDt1qslBqwshXZSKZIqb5yLr1BUu1teJcku1S1keWVammPOwjdkiHNdOl"
-        controls={true}
-      >
-        <source src="http://radiotec.delicias.tecnm.mx:8000/live" />
-        Este elemento no lo soporta el navegador.
-      </audio>
+      <iframe
+        src="http://radiotec.delicias.tecnm.mx/"
+        width="800"
+        height="600"
+      ></iframe>
     </main>
   );
 }
